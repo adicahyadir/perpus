4b49fc7509fd7f8926c7f80d1ed2416b545898ea
Ploye <musa7076@gmail.com>
2021-10-14 08:18:37 +0700

    Ployee

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,9 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+public function __construct()
+	{
+		parent::__construct();
+	
+		$this->load->model('m_bukuz');
+	}
 	public function index()
 	{
-		$isi['title']   = 'Rak Buku';
-		$this->load->view('welcome_message', $isi);
+		$data['title']   = 'Rak Buku';
+		$data['bukuz'] = $this->m_bukuz->get_all();
+			
+		$this->load->view('welcome_message',$data);
 	}
+	public function search(){
+			$keyword = $this->input->post('keyword');
+			$data['bukuz']= $this->m_bukuz->get_bukuz_keyword($keyword);
+			$this->load->view('welcome_message',$data);
+		}
 }
